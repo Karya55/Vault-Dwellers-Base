b6b9ef9294128fc8c7c6a5a7aa62393d4548344c
Karya55 <ukaryaellibes@gmail.com>
2020-11-01 21:07:13 +0300

    register

--- app/src/main/java/com/janfranco/vaultdwellersbase/user_interface/activities/RegisterActivity.java
+++ app/src/main/java/com/janfranco/vaultdwellersbase/user_interface/activities/RegisterActivity.java
@@ -14,6 +14,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         ButterKnife.bind(this);
+        //karyaisthebestbitch
     }
 
 }
